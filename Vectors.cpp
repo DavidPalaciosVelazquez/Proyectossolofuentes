01ddc0e33c5d93353a5688b9a61b9df343f704b7
DavidPalaciosVelazquez <50608701+DavidPalaciosVelazquez@users.noreply.github.com>
2019-06-15 23:25:01 -0500

    Add files via upload

---
+++ Vectors.cpp
@@ -0,0 +1,41 @@
+vectors.cpp
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+	typedef struct alumno {
+	string codigo;
+	string nombre;
+	int edad;
+}talumno;
+
+
+
+int main()
+{
+
+	for (int n = 0; n < 5; n++) {
+		talumno* apt;
+		vector <talumno> listado;
+		apt = new talumno;
+		cout << endl << " Captura datos de alumno" << endl;
+		cout << " Caputura  codigo: ";
+		cin >> apt->codigo;
+		cout << " Captura el nombre: ";
+		cin >> apt->nombre;
+		cout << " Captura edad: ";
+		cin >> apt->edad;
+		listado.push_back(*apt);
+
+
+		cout << endl;
+		for (talumno x : listado) {
+			cout << " codigo: " << x.codigo << "    Nombre: " << x.nombre << "   Edad: " << x.edad << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
